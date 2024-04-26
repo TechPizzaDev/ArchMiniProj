8352614fff5d6349e5466642e49bfae54e0ff812
Kim <110769011+kim-svedberg@users.noreply.github.com>
2024-04-26 14:54:45 +0200

    Can place ingredients on sandwich
    Co-Authored-By: Martin Elovsson <112435323+Elovsson@users.noreply.github.com>
    Co-Authored-By: TechPizza <23627133+TechPizzaDev@users.noreply.github.com>

--- Assets/Scripts/Station Scripts/SandwichManager.cs
+++ Assets/Scripts/Station Scripts/SandwichManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +5,82 @@ using UnityEngine;
 
 public class SandwichManager : MonoBehaviour
 {
+    GameObject objectInHand;
+    Ingredient ingredientInHand;
+
+    public Collider2D placeArea;
+
+    public Dictionary<Ingredient, int> placedIngredients = new();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        RaycastHit2D hit = RayHelper.RaycastFromCamera(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.TryGetComponent(out Ingredient ingredient))
+                {
+                    objectInHand = Instantiate(ingredient.prefab, hit.collider.gameObject.transform, true);
+                    ingredientInHand = ingredient;
+                }
+            }
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            if (objectInHand != null)
+            {
+                if (hit.collider == placeArea && TryPlace(objectInHand))
+                {
+                }
+                else
+                {
+                    Destroy(objectInHand);
+                }
+
+                objectInHand = null;
+                ingredientInHand = null;
+            }
+        }
+
+        if (objectInHand != null)
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = -0.1f;
+            objectInHand.transform.position = pos;
+        }
+
+        //if click on tomato bowl
+        //mouse = tomato 
+        //if press on macka --> place tomato 
+    }
+
+    bool TryPlace(GameObject objToPlace)
+    {
+        if (!objToPlace.TryGetComponent(out PointList pointList))
+        {
+            return true;
+        }
+
+        int ingdCount = placedIngredients.GetValueOrDefault(ingredientInHand, 0);
+        if (ingdCount < pointList.points.Length)
+        {
+            placedIngredients[ingredientInHand] = ingdCount + 1;
+
+            Vector2 point = pointList.points[ingdCount % pointList.points.Length];
+            objToPlace.transform.position = placeArea.transform.position + new Vector3(point.x, point.y, 0);
+            return true;
+        }
+        return false;
     }
 }
