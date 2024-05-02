30ac95c6fd4e5d5359f9a035cceb304a61bcdc29
Kim <110769011+kim-svedberg@users.noreply.github.com>
2024-05-02 15:34:47 +0200

    Almost finished sandwich station
    Co-Authored-By: Martin Elovsson <112435323+Elovsson@users.noreply.github.com>
    Co-Authored-By: TechPizza <23627133+TechPizzaDev@users.noreply.github.com>

--- Assets/Scripts/Station Scripts/SandwichManager.cs
+++ Assets/Scripts/Station Scripts/SandwichManager.cs
@@ -8,14 +8,20 @@ public class SandwichManager : MonoBehaviour
     GameObject objectInHand;
     Ingredient ingredientInHand;
 
-    public Collider2D placeArea;
+    public GameObject visualPrefab;
+    public VisualSandwich currentVisual;
 
-    public Dictionary<Ingredient, int> placedIngredients = new();
+    public Dictionary<Ingredient, List<GameObject>> ingredients = new();
+
+    VisualSandwich SpawnVisual()
+    {
+        return Instantiate(visualPrefab).GetComponent<VisualSandwich>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentVisual = SpawnVisual();
     }
 
     // Update is called once per frame
@@ -30,8 +36,17 @@ public class SandwichManager : MonoBehaviour
             {
                 if (hit.collider.gameObject.TryGetComponent(out Ingredient ingredient))
                 {
-                    objectInHand = Instantiate(ingredient.prefab, hit.collider.gameObject.transform, true);
+                    objectInHand = Instantiate(ingredient.prefab, currentVisual.placeArea.gameObject.transform, true);
                     ingredientInHand = ingredient;
+                }
+                else if (hit.collider.gameObject.TryGetComponent(out TrashCan trash))
+                {
+                    foreach (var list in ingredients.Values)
+                    {
+                        foreach (var obj in list)
+                            Destroy(obj);
+                    }
+                    ingredients.Clear();
                 }
             }
         }
@@ -40,7 +55,7 @@ public class SandwichManager : MonoBehaviour
         {
             if (objectInHand != null)
             {
-                if (hit.collider == placeArea && TryPlace(objectInHand))
+                if (hit.collider == currentVisual.placeArea && TryPlace(objectInHand))
                 {
                 }
                 else
@@ -68,15 +83,34 @@ public class SandwichManager : MonoBehaviour
             return true;
         }
 
-        int ingdCount = placedIngredients.GetValueOrDefault(ingredientInHand, 0);
-        if (ingdCount < pointList.points.Length)
+        if (!ingredients.TryGetValue(ingredientInHand, out var ingdList))
         {
-            placedIngredients[ingredientInHand] = ingdCount + 1;
+            ingdList = new List<GameObject>();
+            ingredients.Add(ingredientInHand, ingdList);
+        }
 
-            Vector2 point = pointList.points[ingdCount % pointList.points.Length];
-            objToPlace.transform.position = placeArea.transform.position + new Vector3(point.x, point.y, 0);
+        if (ingdList.Count < pointList.points.Length)
+        {
+            ingdList.Add(objToPlace);
+
+            Vector2 point = pointList.points[(ingdList.Count - 1) % pointList.points.Length];
+            objToPlace.transform.position = currentVisual.placeArea.transform.position + new Vector3(point.x, point.y, 0);
             return true;
         }
         return false;
+    }
+
+    public void FinishedSandwich()
+    {
+        if (ingredients.Count == 0)
+        {
+            return;
+        }
+
+        currentVisual.isFinished = true;
+        currentVisual.rigidBody.AddForce(new Vector2(0, 1000));
+
+        ingredients = new Dictionary<Ingredient, List<GameObject>>();
+        currentVisual = SpawnVisual();
     }
 }
